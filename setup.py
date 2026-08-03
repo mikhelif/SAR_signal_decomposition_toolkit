62d7877c3ca1af2e2f737fadc94f9c056e8ff7ac
Mourad Ikhelif <mikhelif23@gmail.com>
2026-08-03 22:30:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="sar-subaperture-processor",
+    name="SAR_ignal_decomposition_toolkit",
     version="0.1.0",
-    description="Lightweight Python tool for generating SAR azimuth sub-aperture images from SLC data",
+    description="Python toolkit for SAR signal decomposition of SLC data, including azimuth sub-apertures and range-frequency sub-bands.",
     author="mourad_ikhelif",
     url="https://github.com/mikhelif/sar-subaperture-processor",
     python_requires=">=3.8",
-    py_modules=["sar_subaperture_processor"],
+    py_modules=["SARDecomp"],
     install_requires=[
         "numpy",
         "rasterio",
